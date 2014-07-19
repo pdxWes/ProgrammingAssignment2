df343ac54dcf64d556e842b708f81249735c8bcd
Wes Hoyer <r.wesley.hoyer@gmail.com>
2014-07-19 16:38:22 -0700

    completed R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions allow for calculations on a input to be cached so they
+## can be re-called at later without having to perform the calcualtion again.
+## In this case, we will be using matrix inversion as an example.
 
-## Write a short comment describing this function
+## The first function produces a list of functions that will make the input 
+## cacheable. It is setting up an environment where the defined functions act as
+## methods within the parent function's environment or class. By using the <<-
+## operator within the two set functions, they can "see" and use the variables
+## set in the parent environment rather than be closes off and thought of as
+## completely different variables usable only within the function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y){
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setinversion <- function(inversion) inv <<- inversion
+      getinversion <- function() inv
+      list(set = set, get = get,
+           setinversion = setinversion,
+           getinversion = getinversion)
 }
 
 
-## Write a short comment describing this function
+## This function calls the functions defined in the first function, if the 
+## matrix inversion is already performed, it can access it from the 
+## makeCacheMatrix the getinversion() method, if it is is still a NULL, it will 
+## get the data with get() thenactually perform the calculation with the solve()
+## function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinversion()
+        if(!is.null(inv)) {
+              message("getting cached data")
+              retrun(inv)
+        }
+        data <- x$get()
+        inv <- solve(data,...)
+        x$setinversion(inv)
+        inv
 }
+
+## test the functions
+test <- makeCacheMatrix(matrix(c(22, 1, 44, 7, 2, 5 , 3, 8, 3), 3, 3))
+cacheSolve(test)
