a2df78c44000dbe4e7163780378d6aacaad6c102
Wes Hoyer <r.wesley.hoyer@gmail.com>
2014-07-19 16:54:54 -0700

    fixed typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
 ## This set of functions allow for calculations on a input to be cached so they
-## can be re-called at later without having to perform the calcualtion again.
+## can be re-called later without having to perform the calculation again.
 ## In this case, we will be using matrix inversion as an example.
 
 ## The first function produces a list of functions that will make the input 
 ## cacheable. It is setting up an environment where the defined functions act as
 ## methods within the parent function's environment or class. By using the <<-
 ## operator within the two set functions, they can "see" and use the variables
-## set in the parent environment rather than be closes off and thought of as
-## completely different variables usable only within the function.
+## defined in the parent environment rather than be closed off and thought of as
+## completely different variables usable only within those functions.
 
 makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
@@ -24,11 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function calls the functions defined in the first function, if the 
-## matrix inversion is already performed, it can access it from the 
-## makeCacheMatrix the getinversion() method, if it is is still a NULL, it will 
-## get the data with get() thenactually perform the calculation with the solve()
-## function.
+## This function calls the functions defined and listedin the first function, if 
+## the matrix inversion is already performed, it can access it from the 
+## makeCacheMatrix()'s getinversion() method, if it is is still a NULL, it will 
+## get the data with get() then actually perform the calculation with the
+## solve () function.
 
 cacheSolve <- function(x, ...) {
         inv <- x$getinversion()
